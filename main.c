d7100cc536334d2c31bb8e89a299c184c5531f74
simonegenovesi92 <s.genovesi@studenti.unica.it>
2022-01-17 05:17:26 -0800

    Menu iniziale semi-funzionante e lista di scritte iniziali e finali
    Aggiunta una finestra iniziale prima del funzionamento del gioco con due scritte. Quando selezionate coi tasti FRECCIA SU e GIU sono illuminate. Tasto ENTER non funzionante. Probabilmente c'è qualche errore.
    Inserito inoltre un file txt con scritte di inizio e fine prodotte con Figlet.

--- main.c
+++ main.c
@@ -5,10 +5,88 @@ void main()
 {
     int tubo[2];
     int i = 0 , pidNav, pidPro;
+    int MAXX, MAXY;
+
+    char list[2][18] = {"Inizia partita", "Esci dal gioco"};
+    char item[18];
+    int ch, width = 18;
+
+    //finestre per il menu iniziale di gioco
+    WINDOW *w1;
 
     initscr();
     noecho();
     curs_set(false);
+
+    /* INIZIO MENU */ 
+    getmaxyx(stdscr, MAXY, MAXX);
+
+    w1 = newwin( MAXY / 2 , MAXX - 2 , 1 , 1);
+    box(w1, 0 , 0);
+    //mvwprintw (w1 , 5 , 20 , "BENVENUTI SU SPACE INVADERS.");
+    //mvwprintw (w1 , 6 , 18 , "premi un tasto per continuare...");
+    for ( i = 0; i < 2; i++)
+    {
+        if ( i == 0)
+            wattron( w1, A_STANDOUT);
+        else
+            wattroff( w1, A_STANDOUT);
+            sprintf(item, "%-17s", list[i]);
+            mvwprintw( w1, i+1, 2, "%s", item);
+    }
+    wrefresh( w1 );
+    i = 0;
+    //noecho();
+    keypad( w1, TRUE );
+    curs_set( 0 );
+    
+    while ((ch = wgetch(w1)) != 'q')
+    {
+        sprintf(item, "%-17s", list[i]);
+        mvwprintw( w1, i+1, 2, "%s", item);
+
+        switch (ch)
+        {
+        case KEY_UP:
+            i--;
+            if (i < 0)
+                i = 1;
+            break;
+        
+        case KEY_DOWN:
+            i++;
+            if (i > 1)
+                i = 0;
+            break;
+            
+        case KEY_ENTER:
+            if (i = 0)
+            {
+                delwin( w1 );
+                endwin();
+                break;
+            }
+            else if ( i = 1 )
+            {
+                delwin ( w1 );
+                endwin();
+                exit(0);
+                break;
+
+            }
+        }
+        wattron( w1 , A_STANDOUT );
+        sprintf(item, "%-17s", list[i]);
+        mvwprintw( w1, i+1, 2, "%s", item);
+        wattroff( w1, A_STANDOUT );
+    }    
+
+
+    //wgetch(w1);
+    //delwin;
+
+    /* FINE MENU   */
+
 
     wrefresh(stdscr);
 
